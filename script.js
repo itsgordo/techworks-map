98be551f27fca704abcfedf1cfcef8042215450f
Gordon Liang <itsgordon8@gmail.com>
2024-04-19 17:48:57 -0400

    Updated script.js

--- script.js
+++ script.js
@@ -123,37 +123,26 @@ function myFunc(counties) {
     onEachFeature: onEachFeature,
   }).addTo(map);
 
-  var hoveredLayer = null;
-
   // Mouseover actions
   function onMapMouseOver(e) {
     var layer = e.target;
 
     // Hover highlight if the county is active
-    // Check if the county is active
-   // Check if the county is active
-   if (layer.feature.properties.active) {
-    // Set hover style for hovered active counties
-    layer.setStyle(hoverActiveCountiesOptions);
-    layer.bringToFront();
-    info.update(layer.feature.properties);
-    
-    // Update hoveredLayer
-    hoveredLayer = layer;
-   }
+    if (layer.feature.properties.active) {
+      layer.setStyle(hoverActiveCountiesOptions);
+      layer.bringToFront();
+      info.update(layer.feature.properties);
+    }
   }
 
   // Resets mouseover actions
   function onMapMouseOut(e) {
     var layer = e.target;
 
+    // Reset hover highlight if the county is active
     if (layer.feature.properties.active) {
-      // Reset style for non-hovered active counties
       layer.setStyle(activeCountiesOptions);
       info.update();
-      
-      // Clear hoveredLayer
-      hoveredLayer = null;
     }
   }
 
@@ -190,6 +179,8 @@ function myFunc(counties) {
 
     layer.on({
       mouseover: onMapMouseOver,
+      mousedown: onMapMouseOver,
+      mouseup: onMapMouseOut,
       mouseout: onMapMouseOut,
       click: onMapClick,
     });
